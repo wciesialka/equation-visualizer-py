953aab91a579d3644ea304c5852e24ecef017471
Willow Ciesialka <wciesialka@gmail.com>
2021-12-17 15:39:27 -0500

    Added user control and controlled undefined expressions.

--- src/riemann_visualizer/__main__.py
+++ src/riemann_visualizer/__main__.py
@@ -1,13 +1,16 @@
 import pygame
 import riemann_visualizer.visualizer as vis
-from math import tan
+from math import log, sin
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode([900, 900])
 
-    eq = vis.RiemannSum(lambda x: tan(x), (-3.14, 3.14), (-1, 1))
+    domain = [-1, 1]
+    range = [-1, 1]
+
+    eq = vis.RiemannSum(lambda x: log(x), domain.copy(), range.copy())
     visualizer = vis.Visualizer(eq, screen)
 
     running = True
@@ -15,8 +18,24 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEWHEEL:
+                if event.y != 0:
+                    eq.zoom(event.y)
+            elif event.type == pygame.MOUSEMOTION:
+                # If LMB is down...
+                if event.buttons[0] == 1:
+                    rel = [x/100 for x in event.rel]
+                    eq.domain[0] -= rel[0]
+                    eq.domain[1] -= rel[0]
+                    eq.range[0]  += rel[1]
+                    eq.range[1]  += rel[1]
+            elif event.type == pygame.KEYUP:
+                if event.key == 114: # r
+                    eq.domain = domain.copy()
+                    eq.range = range.copy()
         screen.fill((255, 255, 255))
         visualizer.draw_equation()
+        visualizer.draw_text()
 
         pygame.display.flip()
 
--- src/riemann_visualizer/visualizer.py
+++ src/riemann_visualizer/visualizer.py
@@ -2,28 +2,38 @@
 
 from typing import Callable, Tuple, List
 import pygame
+import logging
 
 class RiemannSum:
 
     def __init__(self, f:Callable[[float], float], domain: Tuple[int, int], range: Tuple[int, int]):
-        self.left, self.right = domain
-        self.top, self.bottom = range
+        self.domain = domain
+        self.range = range
         self.f = f
+
+    def zoom(self, by):
+        if self.range[0] - by/2 < self.range[1] + by/2 and self.domain[0] - by/2 < self.domain[1] + by/2:
+            self.range[0] -=   by/2
+            self.range[1] +=   by/2
+            self.domain[0] -=  by/2
+            self.domain[1] +=  by/2
 
 class Visualizer:
 
     def __init__(self, equation: RiemannSum, screen: pygame.Surface, *, color: Tuple[int, int, int] = (0, 0, 0)):
+        pygame.font.init()
+        self.font = pygame.font.SysFont("Courier New", 12)
         self.equation = equation
         self.screen = screen
         self.color = color
 
     @property
     def left(self):
-        return self.equation.left
+        return self.equation.domain[0]
     
     @property
     def right(self):
-        return self.equation.right
+        return self.equation.domain[1]
 
     @property
     def f(self):
@@ -39,34 +49,53 @@ class Visualizer:
 
     @property
     def top(self):
-        return self.equation.top
+        return self.equation.range[1]
 
     @property
     def bottom(self):
-        return self.equation.bottom
+        return self.equation.range[0]
 
     def draw_equation(self):
 
         dx: float = (self.right-self.left)/self.width
 
         def map(x):
-            y = self.f(x)
-            # output = output_start + ((output_end - output_start) / (input_end - input_start)) * (input - input_start)
-            return self.height - (self.height + ((0 - self.height) / (self.top - self.bottom)) * (y - self.bottom))
+            try:
+                y = self.f(x)
+            except:
+                return None # Error
+            else:
+                # output = output_start + ((output_end - output_start) / (input_end - input_start)) * (input - input_start)
+                divisor = (self.top - self.bottom)
+
+                return (self.height + ((0 - self.height) / divisor) * (y - self.bottom))
 
         list_of_points = []
-        points: List[Tuple[int, float]] = [(0, map(self.left))]
-        for i in range(1,self.width):
+        points: List[Tuple[int, float]] = []
+        for i in range(0,self.width):
             x = self.left + (dx*i)
             y = map(x)
             # If undefined...
-            if abs(y) > self.height * 4:
+            if y is None or abs(y) > self.height * 4:
                 list_of_points.append(points)
                 points = []
+                if y is None:
+                    continue
             point: Tuple[int, float] = (i, y)
             points.append(point)
         list_of_points.append(points)
 
         for points in list_of_points:
             if(len(points) >= 2):
-                pygame.draw.lines(self.screen, self.color, False, points, 1)
+                try:
+                    pygame.draw.lines(self.screen, self.color, False, points, 1)
+                except Exception as ex:
+                    logging.debug(points)
+                    raise ex
+
+    def draw_text(self):
+
+        domain_text = self.font.render(f'Domain: [{self.left}, {self.right}]', True, (0, 0, 0), (255, 255, 255))
+        range_text = self.font.render(f'Range: [{self.bottom}, {self.top}]', True, (0, 0, 0), (255, 255, 255))
+        self.screen.blit(domain_text, (0, 0))
+        self.screen.blit(range_text, (0, 12))
