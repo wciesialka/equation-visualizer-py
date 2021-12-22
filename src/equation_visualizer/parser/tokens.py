9cd4b78346106295519926c26a619b554f48ec2d
Willow Ciesialka <wciesialka@gmail.com>
2021-12-22 16:10:34 -0500

    Completed tokens.

--- src/equation_visualizer/parser/tokens.py
+++ src/equation_visualizer/parser/tokens.py
@@ -3,20 +3,22 @@ from abc import ABC, abstractmethod
 from typing import List, Dict
 from math import e, pi, sin, cos, tan, log10
 
-class TokenBuilder:
-
-    def __init__(self, stack: List):
-        self.__stack = stack
-
-    def build_value(self, value: float):
-        return ValueToken(self.__stack, value)
-
 class Token(ABC):
 
     def __init__(self, stack: List):
         self.__stack = stack
 
     @abstractmethod
+    def execute(self):
+        pass
+
+class OpenToken(Token):
+
+    def execute(self):
+        pass
+
+class CloseToken(Token):
+
     def execute(self):
         pass
 
@@ -107,7 +109,54 @@ class LogToken(UnaryToken):
     def operation(self, a):
         return log10(a)
 
-TOKEN_REGEX = re.compile(r"\d+.?\d*|\+|-|\*|\(|\)|\^|tan|cos|sin|log|pi|e")
+class TokenBuilder:
+
+    def __init__(self, stack: List):
+        self.__stack = stack
+
+    def build_value(self, value: float):
+        return ValueToken(self.__stack, value)
+
+    def build_add(self):
+        return AddToken(self.__stack)
+
+    def build_subtract(self):
+        return SubtractToken(self.__stack)
+
+    def build_multiply(self):
+        return MultiplyToken(self.__stack)
+
+    def build_divide(self):
+        return DivideToken(self.__stack)
+
+    def build_power(self):
+        return PowerToken(self.__stack)
+
+    def build_pi(self):
+        return PiToken(self.__stack)
+
+    def build_e(self):
+        return EulerToken(self.__stack)
+
+    def build_sin(self):
+        return SinToken(self.__stack)
+
+    def build_cos(self):
+        return CosToken(self.__stack)
+
+    def build_tan(self):
+        return TanToken(self.__stack)
+
+    def build_log(self):
+        return LogToken(self.__stack)
+
+    def build_open(self):
+        return OpenToken(self.__stack)
+
+    def build_close(self):
+        return CloseToken(self.__stack)
+
+TOKEN_REGEX = re.compile(r"\d+\.?\d*|\+|-|\*|\(|\)|\^|tan|cos|sin|log|pi|e")
 
 def tokenize(expression: str, stack: List[float]) -> List[Token]:
     '''Tokenize a string.
@@ -120,6 +169,19 @@ def tokenize(expression: str, stack: List[float]) -> List[Token]:
     builder: TokenBuilder = TokenBuilder(stack)
 
     REPRESENTATIONS: Dict[str, Token] = {
+        "+": builder.build_add,
+        "-": builder.build_subtract,
+        "*": builder.build_multiply,
+        "/": builder.build_divide,
+        "^": builder.build_power,
+        "pi": builder.build_pi,
+        "e": builder.build_e,
+        "sin": builder.build_sin,
+        "cos": builder.build_cos,
+        "tan": builder.build_tan,
+        "log": builder.build_log,
+        "(": builder.build_open,
+        ")": builder.build_close
     }
 
     matches: List = TOKEN_REGEX.findall(expression)
@@ -136,4 +198,4 @@ def tokenize(expression: str, stack: List[float]) -> List[Token]:
             else:
                 tokens.append(builder.build_value(x))
 
-    return tokens
+    return (tokens, matches)
